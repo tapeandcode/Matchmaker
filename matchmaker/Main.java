621ae846f9b00da51203bb8ba48295d7a8bf6715
Travis Olbrich <travis@tapeandcode.com>
2010-10-20 23:24:14 -0400

    Fixed comment error

--- matchmaker/Main.java
+++ matchmaker/Main.java
@@ -18,7 +18,6 @@
  * 
  * You should have received a copy of the GNU General Public License
  * along with Matchmaker.  If not, see <http ://www.gnu.org/licenses/>.
-< #if licenseLast??>
  */
 
 package matchmaker;
