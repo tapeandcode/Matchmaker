719b47752835dc8a2f97a068b51446b790078641
Travis Olbrich <travis@tapeandcode.com>
2010-10-20 23:22:41 -0400

    Added GPL stuff

--- matchmaker/Main.java
+++ matchmaker/Main.java
@@ -1,13 +1,31 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+
+ * Main.java
+ * 
+ * Copyright (c) 2010 Travis Olbrich.
+ * 
+ * This file is part of Matchmaker.
+ * 
+ * Matchmaker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Matchmaker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Matchmaker.  If not, see <http ://www.gnu.org/licenses/>.
+< #if licenseLast??>
  */
 
 package matchmaker;
 
 /**
  *
- * @author travis
+ * @author Travis Olbrich <travis at tapeandcode.com>
  */
 public class Main {
 
